d4a8dce500cd3cfa83dd77c7ae921bfc937587e2
HerediaLeo <lheredia1195@gmail.com>
2020-07-15 18:39:06 -0300

    Projeto Colab

---
+++ BackendJuniorLeonardoHeredia/BackendJuniorLeonardoHeredia/src/main/java/br/com/project/BackendJuniorLeonardoHeredia/BackendJuniorLeonardoHerediaApplication.java
@@ -0,0 +1,13 @@
+package br.com.project.BackendJuniorLeonardoHeredia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJuniorLeonardoHerediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendJuniorLeonardoHerediaApplication.class, args);
+	}
+
+}
